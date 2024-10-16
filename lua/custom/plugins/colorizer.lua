ba4e258404de4bd97fba787bb62d483339963a43
Francisco Espino <fran@cofi.ai>
2024-10-16 21:26:30 +0200

    feat(colorizer): add toggle shortcut

--- lua/custom/plugins/colorizer.lua
+++ lua/custom/plugins/colorizer.lua
@@ -7,6 +7,21 @@ return {
           tailwind = true,
         },
       }
+
+      -- Add keymap to toggle colorizer
+      local colorizer_active = true
+      vim.api.nvim_set_keymap('n', '<leader>tc', ':lua ToggleColorizer()<CR>', { noremap = true, silent = true, desc = '[T]oggle [C]olorizer' })
+
+      -- Toggle colorizer function
+      function ToggleColorizer()
+        if colorizer_active then
+          vim.cmd 'ColorizerDetachFromBuffer'
+          colorizer_active = false
+        else
+          vim.cmd 'ColorizerAttachToBuffer'
+          colorizer_active = true
+        end
+      end
     end,
   },
 }
